0b28c107311ab946db593ebc537ff184936557b2
siwan <pswan3168@gmail.com>
2021-03-28 00:56:10 +0900

    complete : Reusable Info Section + Styling

---
+++ src/components/infoSection/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Button } from 'react-scroll';
+import {
+  InfoContainer,
+  InfoWrapper,
+  InfoRow,
+  Column1,
+  Column2,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  BtnWrap,
+  ImgWrap,
+  Img,
+} from './InfoElements';
+
+const InfoSection = () => {
+  return (
+    <>
+      <infoContainer>
+        <InfoWrapper>
+          <InfoRow>
+            <Column1>
+              <TextWrapper>
+                <TopLine>TopLine</TopLine>
+                <Heading>Heading</Heading>
+                <Subtitle>Subtitle</Subtitle>
+                <BtnWrap>
+                  <Button to='home' />
+                </BtnWrap>
+              </TextWrapper>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <Img />
+              </ImgWrap>
+            </Column2>
+          </InfoRow>
+        </InfoWrapper>
+      </infoContainer>
+    </>
+  );
+};
+
+export default InfoSection;
